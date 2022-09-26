8d3f3b39c381d1764fadad0dc44ed3b43a8a8d92
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-09-26 17:50:21 +0530

    Added:CRUD operations on data

--- src/main/java/com/emppayroll/conttroller/EmployeeController.java
+++ src/main/java/com/emppayroll/conttroller/EmployeeController.java
@@ -4,6 +4,9 @@ import com.emppayroll.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/emp")
 public class EmployeeController {
@@ -23,9 +26,34 @@ public class EmployeeController {
         return service.welcomeMessage();
     }
 
+    //Insert Employee Data
     @PostMapping("/post")
-    public EmployeeEntity addEmpData(@RequestBody EmployeeEntity empData) {
+    public EmployeeEntity addData(@RequestBody EmployeeEntity empData) {
         service.saveData(empData);
         return empData;
+    }
+
+    //Get by ID
+    @GetMapping("/id/{id}")
+    public Optional<EmployeeEntity> getById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    //get all the data
+    @GetMapping("/all")
+    public List<EmployeeEntity> findAllData() {
+        return service.findAllData();
+    }
+    //Edit or Update the data by id
+    @PutMapping("/edit/{id}")
+    public EmployeeEntity editData(@RequestBody EmployeeEntity empData, @PathVariable long id) {
+        return service.editData(empData, id);
+    }
+
+    //Delete the data by id
+    @DeleteMapping("/delete/{id}")
+    public String deleteData(@PathVariable Long id) {
+        service.deleteData(id);
+        return "Employee Data deleted";
     }
 }
--- src/main/java/com/emppayroll/service/EmployeeService.java
+++ src/main/java/com/emppayroll/service/EmployeeService.java
@@ -4,6 +4,9 @@ import com.emppayroll.repo.Repo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
     @Autowired
@@ -16,5 +19,31 @@ public class EmployeeService {
     public EmployeeEntity saveData(EmployeeEntity empData) {
         repository.save(empData);
         return empData;
+    }
+    public Optional<EmployeeEntity> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<EmployeeEntity> findAllData() {
+        return repository.findAll();
+    }
+
+    public EmployeeEntity editData(EmployeeEntity empData, Long id) {
+        EmployeeEntity existingGreet = repository.findById(id).orElse(null);
+        if (existingGreet != null) {
+            existingGreet.setEmployeeId(empData.getEmployeeId());
+            existingGreet.setFirstName(empData.getFirstName());
+            existingGreet.setLastName(empData.getLastName());
+            existingGreet.setProfilePic(empData.getProfilePic());
+            existingGreet.setAddress(empData.getAddress());
+            existingGreet.setSalary(empData.getSalary());
+            existingGreet.setNotes(empData.getNotes());
+            return repository.save(existingGreet);
+        }
+        else
+            return null;
+    }
+    public void deleteData(Long id) {
+        repository.deleteById(id);
     }
 }
