954dd42c4e77b3064ba174cc3bbe66ab3633c67c
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-09-28 21:16:11 +0530

    Created:Employee Payroll App

--- src/main/java/com/emppayroll/EmpPayrollApplication.java
+++ src/main/java/com/emppayroll/EmpPayrollApplication.java
@@ -1,14 +1,15 @@
 package com.emppayroll;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmpPayrollApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmpPayrollApplication.class, args);
-        System.out.println("Employee Payroll Project");
+        log.info("Employee Payroll App");
     }
-
 }
