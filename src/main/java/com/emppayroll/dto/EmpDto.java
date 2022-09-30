31465aa17533dd3f7be72357c2548351b516a895
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-09-30 17:53:24 +0530

    Added: Validation to fields

--- src/main/java/com/emppayroll/dto/EmpDto.java
+++ src/main/java/com/emppayroll/dto/EmpDto.java
@@ -1,9 +1,11 @@
 package com.emppayroll.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
@@ -11,12 +13,23 @@ import java.util.Date;
 public class EmpDto {
     @Id
     @GeneratedValue
-    long employeeId;
+
+    Long employeeId;
+
+    @NotEmpty (message = "First name can not be empty")
+    @Pattern(regexp="^[A-Z][a-zA-Z\\s]{2,}$", message="Invalid Name(Should have 1 Upper Case and min 3 Characters.)")
     String firstName;
+    @NotEmpty (message = "Last name can not be empty")
+    @Pattern(regexp="^[A-Z][a-zA-Z\\s]{2,}$", message="Invalid Name(Should have 1 Upper Case and min 3 Characters.)")
     String lastName;
+    @NotEmpty(message = "Address Cannot be Empty")
     String address;
     String profilePic;
+    @Min(value = 5000, message="Salary Should be More than 5000")
     Long salary;
+    @JsonFormat(pattern = "dd mm yyyy")
+    @NotNull(message = "Start Date cannot be Empty")
+    @PastOrPresent(message = "Start Date should be past or present date")
     Date startDate;
     String notes;
 
