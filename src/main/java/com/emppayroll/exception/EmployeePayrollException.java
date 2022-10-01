12f03c3bc908190675272096e24519c0b9bfefdf
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-10-01 11:05:18 +0530

    Added:Exceptions if validations fail

---
+++ src/main/java/com/emppayroll/exception/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.emppayroll.exception;
+
+public class EmployeePayrollException extends RuntimeException{
+    public EmployeePayrollException(String message) {
+        super (message);
+    }
+}
---
+++ src/main/java/com/emppayroll/exception/EmployeePayrollHandler.java
@@ -0,0 +1,21 @@
+package com.emppayroll.exception;
+
+import com.emppayroll.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class EmployeePayrollHandler {
+    @ExceptionHandler(EmployeePayrollException.class)
+    public ResponseEntity<ResponseDto> handleEmployeePayrollException(EmployeePayrollException exception){
+        ResponseDto resDTO = new ResponseDto("Exception while processing REST request",
+                exception.getMessage());
+        return new ResponseEntity(resDTO, HttpStatus.BAD_REQUEST);
+    }
+}
